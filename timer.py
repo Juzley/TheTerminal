00900e54db868ec27f3a0fa3affe9eac2674ea2b
Juzley <juzley@gmail.com>
2016-11-05 08:04:29 +0000

    Add timer class and start adding countdown timer to gameplay

---
+++ timer.py
@@ -0,0 +1,29 @@
+"""Timer module."""
+
+import pygame
+
+
+class Timer:
+
+    """Timer class."""
+
+    def __init__(self):
+        """Initialize the class."""
+        self.reset()
+
+    def reset(self):
+        """Reset the timer."""
+        self._lasttime = pygame.time.get_ticks()
+        self.paused = False
+        self.time = 0
+        self.frametime = 0
+
+    def update(self):
+        """Update the time values based on the current tickcount."""
+        time = pygame.time.get_ticks()
+
+        if not self._paused:
+            self.frametime = time - self._lasttime
+            self.time += self.frametime
+
+        self._lasttime = time
